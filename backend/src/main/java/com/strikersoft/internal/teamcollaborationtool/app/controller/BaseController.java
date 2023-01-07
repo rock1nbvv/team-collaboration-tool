5f7b945614beb1de0888f8bc199aeb4827df5264
Vlad Baklaiev <vlad.baklaiev@strikersoft.com>
2023-01-07 14:41:11 +0200

    add backend and frontend modules

---
+++ backend/src/main/java/com/strikersoft/internal/teamcollaborationtool/app/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.strikersoft.internal.teamcollaborationtool.app.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Vlad Baklaiev
+ */
+@Controller
+public class BaseController {
+}
